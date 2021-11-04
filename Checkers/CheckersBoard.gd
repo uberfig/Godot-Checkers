1d704452499536ff39bdf97ae9326839405361f6
uberfig <aidan.gifford04@gmail.com>
2021-11-04 19:22:54 -0400

    finished move function and new game function

--- Checkers/CheckersBoard.gd
+++ Checkers/CheckersBoard.gd
@@ -8,23 +8,39 @@ var current_board: Dictionary = {}
 onready var w_peices_remaining: int = $W.get_child_count()
 onready var b_peices_remaining: int = $B.get_child_count()
 
-# Called when the node enters the scene tree for the first time.
+var selecting_destination := false
+var selected_tile: Vector2
+
+#if true it is black's turn if false it is white's turn
+var current_turn:= true
+
+onready var white_team = preload("res://Checkers/WhiteTeam.tscn")
+onready var black_team = preload("res://Checkers/BlackTeam.tscn")
+onready var white_team_ref =$W
+onready var black_team_ref =$B
+
+
 func _ready():
-	for y in 7:
-		for x in 7:
-			current_board[Vector2(x, y)] = [0, null]
+	empty_board()
 	
 	update_board()
 	
-	move_peice(Vector2(0,2), Vector2(2,4))
+#	print(current_board)
+	
+#	move_peice(Vector2(0,2), Vector2(2,4))
 
+
+func empty_board():
+	for y in 8:
+		for x in 8:
+			current_board[Vector2(x, y)] = [0, null]
 
 
 func update_board():
-	b_peices_remaining = $B.get_child_count()
-	w_peices_remaining = $W.get_child_count()
+	b_peices_remaining = black_team_ref.get_child_count()
+	w_peices_remaining = white_team_ref.get_child_count()
 	
-	var b_children = $B.get_children()
+	var b_children = black_team_ref.get_children()
 	for b_child in b_children:
 		var position = b_child.get_position()
 		var coord: Vector2 = $Board.world_to_map(position)
@@ -37,7 +53,7 @@ func update_board():
 		else:
 			print("there is a weird checker at: ", coord)
 	
-	var w_children = $W.get_children()
+	var w_children = white_team_ref.get_children()
 	for w_child in w_children:
 		var position = w_child.get_position()
 		var coord: Vector2 = $Board.world_to_map(position)
@@ -50,9 +66,11 @@ func update_board():
 		else:
 			print("there is a weird checker at: ", coord)
 
+
 func move_peice(initial_coord: Vector2, destination: Vector2):
-	if(current_board[initial_coord][0] == 0):
-		return(0)
+	if(current_board[destination][0] != 0):
+		print("tile is filled")
+		return
 	else:
 #		move the peice
 		var current_pos = ($Board.map_to_world(initial_coord) + Vector2(32,32))
@@ -67,3 +85,48 @@ func move_peice(initial_coord: Vector2, destination: Vector2):
 		current_board[initial_coord][0] = 0
 		current_board[initial_coord][1] = null
 
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton && event.is_pressed():
+		var world_click_pos = event.position
+		var map_cell_pos = $Board.world_to_map(world_click_pos)
+		
+		if((map_cell_pos.x >= 8) or (map_cell_pos.y >= 8)):
+			if(selecting_destination == true):
+				selecting_destination = false
+			return
+		
+		if((current_board[map_cell_pos][0] == 0) && (selecting_destination == false)):
+			return
+		
+		if selecting_destination == false:
+			selecting_destination = true
+			selected_tile = map_cell_pos
+		else:
+			if selected_tile == map_cell_pos:
+				selecting_destination = false
+			else:
+				move_peice(selected_tile, map_cell_pos)
+				selecting_destination = false
+
+
+func new_game():
+	empty_board()
+	
+	white_team_ref.queue_free()
+	black_team_ref.queue_free()
+	
+	var w_instance = white_team.instance()
+	white_team_ref = w_instance
+	add_child(w_instance)
+	
+	var b_instance = black_team.instance()
+	black_team_ref = b_instance
+	add_child(b_instance)
+	
+	update_board()
+	current_turn = true
+
+
+func _on_Button_pressed():
+	new_game()
