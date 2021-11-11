cd77de949d6c05af13b4949ac90c03e07ddba474
uberfig <aidan.gifford04@gmail.com>
2021-11-10 21:43:47 -0500

    almost finished movement; I did a large amount of work on a weak device that couldn't teat the code written so now I have a lot of debugging to do

--- Checkers/CheckersBoard.gd
+++ Checkers/CheckersBoard.gd
@@ -11,6 +11,7 @@ var current_board: Dictionary = {}
 var selecting_destination := false
 var selected_tile: Vector2
 var public_viable_locations:= {}
+#contents will be formated {destination: [is_jumping, starting_point, jumped_tile(if applicable)]}
 
 #if true it is black's turn if false it is white's turn
 var current_turn:= true
@@ -70,12 +71,6 @@ func update_board():
 
 
 func move_peice(initial_coord: Vector2, destination: Vector2):
-#	if(current_turn == true):
-#		if((current_board[initial_coord][0] != 2) or (current_board[initial_coord][0] != 4)):
-#			return
-#	else:
-#		if((current_board[initial_coord][0] != 1) or (current_board[initial_coord][0] != 3)):
-#			return
 	
 	if(current_board[destination][0] == true):
 		print("tile is filled")
@@ -95,30 +90,37 @@ func move_peice(initial_coord: Vector2, destination: Vector2):
 		current_board[initial_coord][1] = null
 	
 	public_viable_locations = {}
+#contents will be formated {destination: [is_jumping, starting_point, jumped_tile(if applicable)]}
 
 
 func _unhandled_input(event):
-	if event is InputEventMouseButton && event.is_pressed():
+	if(event.is_action_pressed("click")):
 		var world_click_pos = event.position
 		var map_cell_pos = $Board.world_to_map(world_click_pos)
+		
 		
 		if((map_cell_pos.x >= 8) or (map_cell_pos.y >= 8) or (map_cell_pos.x < 0) or (map_cell_pos.y < 0)):
 			if(selecting_destination == true):
 				selecting_destination = false
 			return
-		
+
 		if((current_board[map_cell_pos][0] == false) && (selecting_destination == false)):
 			return
-		
+
 		if selecting_destination == false:
 			selecting_destination = true
-			selected_tile = map_cell_pos
+#			print("selecting destination")
+			position_move_data(map_cell_pos)
+#			print("public_viable_locations: ", public_viable_locations)
+			show_possible_moves()
 		else:
-			if selected_tile == map_cell_pos:
-				selecting_destination = false
-			else:
-				move_peice(selected_tile, map_cell_pos)
-				selecting_destination = false
+			selecting_destination = false
+			clear_move_markers()
+#			if selected_tile == map_cell_pos:
+#				selecting_destination = false
+#			else:
+#				move_peice(selected_tile, map_cell_pos)
+#				selecting_destination = false
 
 
 func new_game():
@@ -135,6 +137,9 @@ func new_game():
 	black_team_ref = b_instance
 	add_child(b_instance)
 	
+	for child in $ViableLocations.get_children():
+		child.queue_free()
+	
 	update_board()
 	current_turn = true
 
@@ -142,8 +147,9 @@ func new_game():
 func _on_NewGame_pressed():
 	new_game()
 
+
 #true is black, false is white
-func can_jump(check_position: Vector2):
+func position_move_data(check_position: Vector2):
 	
 	var team = is_tile_filled(check_position)[1]
 	var is_king = is_tile_filled(check_position)[2]
@@ -151,42 +157,75 @@ func can_jump(check_position: Vector2):
 	#we will spawn a marker at each viable location which when cliked will
 	#move the peice to that location and delete any jumped tile
 	var viable_locations := {}
-	#contents will be formated {destination: [is_jumping, jumped_tile(if applicable)]}
+	#contents will be formated {destination: [is_jumping, starting_point, jumped_tile(if applicable)]}
 	
 	var directions_to_check:= []
 	
 	if(is_king == false):
 		#black team
 		if(team == "black"):
-			directions_to_check = [Vector2(1,1), Vector2(-1,1)]
+			directions_to_check = [Vector2(1,-1), Vector2(-1,-1)]
 		#white
 		if(team == "white"):
-			directions_to_check = [Vector2(1,-1), Vector2(-1,-1)]
+			directions_to_check = [Vector2(1,1), Vector2(-1,1)]
 	
 	if(is_king == true):
 		directions_to_check = [Vector2(1,1), Vector2(-1,1), Vector2(1,-1), Vector2(-1,-1)]
 	
+#	print("directions to check: ", directions_to_check)
+	
 	for direction in directions_to_check:
+#		print("checking direction: ", direction)
 		var adjacent_data = check_adjacent_for_move(check_position, direction)
+#		print("adjacent data: ", adjacent_data)
 		#in format [can_move, can_jump, [tile, adjacent_tile, jump_tile]]
-		if(adjacent_data[0] == false):
-			break
+#		if(adjacent_data[0] == false):
+#			print("breaking because adjacent_data = ", adjacent_data)
+#			print("at direction: ", direction)
+#			break
 		
 		if((adjacent_data[0] == true) and (adjacent_data[1] == false)):
-			viable_locations[adjacent_data[2][1]] = [false]
+			viable_locations[adjacent_data[2][1]] = [false, check_position]
+#			print("can move but not jump, adjacent data is: ", adjacent_data)
 		
 		if((adjacent_data[0] == true) and (adjacent_data[1] == true)):
-			viable_locations[adjacent_data[2][2]] = [true, adjacent_data[2][1]]
+			viable_locations[adjacent_data[2][2]] = [true, check_position, adjacent_data[2][1]]
+#			print("can jump but not move, adjacent data is: ", adjacent_data)
 	
+#	print("viable_locations: ", viable_locations)
 	public_viable_locations = viable_locations
 
 
 func show_possible_moves():
-	for position in 
-	var marker_instance = move_marker.instance()
-	marker_instance.set_positio
-	var position: Vector2 = $Board.world_to_map(position)
-	add_child(marker_instance)
+	for coord in public_viable_locations:
+		var world_position: Vector2 = ($Board.map_to_world(coord) + Vector2(32, 32))
+		var marker_instance = move_marker.instance()
+		
+		marker_instance.set_position(world_position)
+		$ViableLocations.add_child(marker_instance)
+		marker_instance.connect("clicked", self, "on_move_marker_pressed")
+#		print("instanced and connected marker")
+
+
+func on_move_marker_pressed(refrence):
+	var coord = $Board.world_to_map(refrence.get_position())
+	var coord_data = public_viable_locations[coord]
+	if(coord_data[0] == false):
+		move_peice(coord_data[1], coord)
+	
+	if(coord_data[0] == true):
+		move_peice(coord_data[1], coord)
+		kill_checker(coord_data[2])
+	
+	selecting_destination = false
+	clear_move_markers()
+	public_viable_locations = {}
+
+
+func clear_move_markers():
+	for child in $ViableLocations.get_children():
+		child.queue_free()
+#	print("cleared move markers")
 
 
 func kill_checker(tile):
@@ -201,27 +240,42 @@ func check_adjacent_for_move(tile: Vector2, vector_direction: Vector2):
 	var jump_tile: Vector2 = (tile + (vector_direction * 2))
 	var my_color: String = current_board[tile][2]
 	
+#	print("checking adjacent to: ", tile, " with direction: ", vector_direction)
+#	print("adjacent tile: ", adjacent_tile, " jump tile: ", jump_tile, " my color: ", my_color)
+	
 	if(
-		not((adjacent_tile.x >= 8) or (adjacent_tile.y >= 8) or (adjacent_tile.x < 0) or (adjacent_tile.y < 0))
+		((adjacent_tile.x >= 8) or (adjacent_tile.y >= 8) or (adjacent_tile.x < 0) or (adjacent_tile.y < 0))
 	):
+#		print("outside board")
 		return([false]) #the tile is outside of the board; cannot move this way
+		
 	
 	if(current_board[adjacent_tile][0] == false):
+#		print("adjacent is empty")
 		return([true, false, [tile, adjacent_tile]])#the tile is within the board and empty
+		
 	
 	if(current_board[adjacent_tile][2] == my_color):
+#		print("adjacent is friendly")
+#		print("adjacent color: ", current_board[adjacent_tile][2], " | my color: ", my_color)
+#		print("-------------------current board----------------------")
+#		print(current_board)
+#		print("-------------------current board----------------------")
 		return([false])#the tile is filled with a friendly; cannot move this way
+		
 	
 	if(current_board[adjacent_tile][2] != my_color):
 		if(current_board[jump_tile][0] == false):
+#			print("can jump")
 			return([true, true, [tile, adjacent_tile, jump_tile]])
 			#the tile is filled with an enemy and there isn't a unit behind it; can jump
 		else:
+#			print("jump is guarded")
 			return([false])#the tile is filled with a guarded enemy
 
 
 func is_tile_filled(tile: Vector2):
-	if(current_board[tile][0] == 0):
+	if(current_board[tile][0] == false):
 		return([false, current_board[tile][2], current_board[tile][3]])
 	else:
 		return([true, current_board[tile][2], current_board[tile][3]])
---
+++ Checkers/PossibleMoveMarker.gd
@@ -0,0 +1,15 @@
+extends KinematicBody2D
+
+signal clicked(refrence)
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+
+
+
+func _on_PossibleMoveMarker_input_event(viewport, event, shape_idx):
+	if(event.is_action_pressed("click")):
+		print("movemarker clicked")
+		emit_signal("clicked", self)
